d2d5f9e9704c5262ded14ef7ea671b4f5bc62465
sk-birder <kibe.shuhei@gmail.com>
2026-09-08 23:44:36 +0900

    Section2 13-15

--- 02/index.js
+++ 02/index.js
@@ -7,3 +7,19 @@ console.log(count);
 
 // letは再代入可能だが、同じ名前での再宣言は出来ない
 // let count;
+
+// constは再代入・再宣言は出来ない。また、初期値が必須になる
+// 再代入の必要がないものは原則constを使う。この値は変わらない、と意図がわかりやすいコードになる。
+// const daysInweek; // エラーになる
+const daysInWeek = 7;
+
+// 変数の命名規則
+// lowerCamelCaseがベストプラクティス。$と_も使用可能。
+let tomatoCount;
+// 数字を先頭にすることは出来ない。
+// let 7tomatoCount;
+
+// 算術演算子
+// ほぼJavaと同じだが、累乗を使える
+const resultExponent = 2 ** 3;
+console.log(resultExponent);
