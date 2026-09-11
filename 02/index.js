aa51828031660674db297b35e2fb6e6a9e5207b3
sk-birder <kibe.shuhei@gmail.com>
2026-09-11 23:29:04 +0900

    Section2 16-18

--- 02/index.js
+++ 02/index.js
@@ -23,3 +23,22 @@ let tomatoCount;
 // ほぼJavaと同じだが、累乗を使える
 const resultExponent = 2 ** 3;
 console.log(resultExponent);
+
+// インクリメント
+// 前置と後置
+let numInc = 1;
+console.log(numInc++); // 1が返った後に演算する
+console.log(++numInc); // 1を加算した後に値を返すため、3が返る
+// 変なところで改行を入れると意図しない挙動になるため注意すること
+
+// バッククォートを用いて変数呼び出しを含めた文字列を作る
+// 変数の呼び出しは${変数名}と記述する。
+// エスケープシーケンス\も使用可能。Javaとほぼ同じ。
+count = 100;
+let message = `あなたは\'${count}\'人目のお客様です`;
+console.log(message);
+// バッククォート内の改行はそのまま改行として扱われる。半角スペースも表示される。
+message = `1行目
+  2行目
+3行目`;
+console.log(message);
